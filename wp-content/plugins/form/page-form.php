898424dec9fdf2743860ee6944dcddeb33469fc8
PussInDasBoot <mroberts@adacado.com>
2017-12-11 18:37:36 +0100

    Removed bootstrap and started styling from scratch

--- wp-content/plugins/form/page-form.php
+++ wp-content/plugins/form/page-form.php
@@ -2,25 +2,30 @@
 /**
 * Plugin Name: form */
 
-// Register bootstrap and jquery
-wp_enqueue_script('jquery');
-wp_register_script('prefix_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js');
-wp_enqueue_script('prefix_bootstrap');
-wp_register_style('prefix_bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
-wp_enqueue_style('prefix_bootstrap');
+add_action('init', 'register_my_script');
+add_action('wp_footer', 'print_my_script');
+
+function register_my_script() {
+    wp_register_script('tax-credit', plugins_url('/js/tax-credit.js', __FILE__ ));
+}
+
+function print_my_script() {
+    global $add_my_script;
+
+    if ( ! $add_my_script )
+        return;
+
+    wp_print_scripts('tax-credit');
+}
+
+wp_register_script('tax-credit', plugins_url('/js/tax-credit.js', __FILE__ ));
+wp_enqueue_script('tax-credit');
+
+wp_register_style('tax-credit', plugins_url('/css/tax-credit.css', __FILE__), array(), '1', 'all');
+wp_enqueue_style( 'tax-credit' );
 
 function custom_registration_function() {
-    echo '
-    <style>
-    div {
-        margin-bottom:2px;
-    }
-     
-    input{
-        margin-bottom:4px;
-    }
-    </style>
-    ';
+
  
     echo '
     <p>This tool will create documents that will help you and your health care provider complete the application for the Disability Tax Credit, commonly referred to as the DTC.</p>
@@ -34,52 +39,39 @@ function custom_registration_function() {
     <p>Do you have any restrictions in the following health areas? Complete all sections that apply:</p>
 
 
-    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-      <div class="panel panel-default">
-        <div class="panel-heading" role="tab" id="Vision">
-          <h4 class="panel-title">
-            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#vision" aria-expanded="true" aria-controls="vision">
-              Vision
-            </a>
-          </h4>
-        </div>
-        <div id="vision" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="Vision">
-          <div class="panel-body">
-            <p>
-                Do you have medical conditions or diagnoses that restricts your vision? Please list all if more than one.
-                <input type="text"></input>
-            </p>
-            <p>
-                Please share what restrictions you experience even with corrective devices, medication or therapy.
-                <div class="radio">
-                  <label>
-                    <input type="radio" name="visionRestrictions" id="legallyBlind" value="legallyBlind">
-                    I am legally blind, with a visual acuity of 20/200 or less with the Snellen Chart or field of vision in both eyes is 20 degrees or less.
-                  </label>
-                </div>
-                <div class="radio">
-                  <label>
-                    <input type="radio" name="visionRestrictions" id="severeRestrictions" value="severeRestrictions">
-                    I am not legally blind. I have less severe restrictions in my vision. 
-                    Explain: 
-                    <input type="text" id="severeRestrictionsExplain"></input>
-                  </label>
-                </div>
-            </p>
-          </div>
-        </div>
-      </div>
+    <button class="accordion">Section 1</button>
+    <div class="panel">
+      <p>Lorem ipsum...</p>
+    </div>
+
+    <button class="accordion">Section 2</button>
+    <div class="panel">
+      <p>Lorem ipsum...</p>
+    </div>
+
+    <button class="accordion">Section 3</button>
+    <div class="panel">
+      <p>Lorem ipsum...</p>
     </div>
     ';
 }
+
+// function form() {
+
+// }
 
 // Register a new shortcode: [cr_custom_registration]
 add_shortcode( 'cr_custom_registration', 'custom_registration_shortcode' );
  
 // The callback function that will replace [book]
 function custom_registration_shortcode() {
+    global $add_my_script;
+
+    $add_my_script = true;
     ob_start();
     custom_registration_function();
     return ob_get_clean();
 }
+
+
 ?>
