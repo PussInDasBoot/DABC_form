964158da304a4548c7f60a53f32d002108fd1862
PussInDasBoot <mroberts@adacado.com>
2018-01-13 13:06:53 +1100

    Refactored showAndHideTargets function

--- wp-content/plugins/tax-credit/speaking.php
+++ wp-content/plugins/tax-credit/speaking.php
@@ -65,6 +65,113 @@ function speaking_registration_function() {
                     </div>
                 </td>
             </tr>
+            <tr>
+                <td>
+                    When you speak with your doctor, your doctor must ask you to repeat words and sentences. How long does it take to communicate your needs?
+                </td>
+                <td>
+                    <div class="radio">
+                        <input type="radio" name="speechDrComm" id="speechDrCommThree" value="three">
+                        <label for="speechDrCommThree">
+                            At least three times as long, compared to an average person my age without my restrictions.
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechDrComm" id="speechDrCommTwo" value="two">
+                        <label for="speechDrCommTwo">
+                            At least two times as long, compared to an average person my age without my restrictions.
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechDrComm" id="speechDrCommLittle" value="little">
+                        <label for="speechDrCommLittle">
+                            A little bit longer, compared to an average person my age without my restrictions.
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechDrComm" id="speechDrCommSame" value="same">
+                        <label for="speechDrCommSame">
+                            The same amount of time, compared to an average person my age without my restrictions.
+                        </label>
+                    </div>
+                <td>
+            </tr>
+            <tr>
+                <td>
+                    What happens when you speak?
+                </td>
+                <td>
+                    <div class="radio">
+                        <input type="radio" name="speechWhatHappens" id="speechWhatHappensVol" value="vol">
+                        <label for="speechWhatHappensVol">
+                            Low-volume
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechWhatHappens" id="speechWhatHappensSlur" value="slur">
+                        <label for="speechWhatHappensSlur">
+                            Slurring
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechWhatHappens" id="speechWhatHappensStut" value="stut">
+                        <label for="speechWhatHappensStut">
+                            Stuttering
+                        </label>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Do you use any appropriate therapy, medication or devices? If yes, please describe.
+                </td>
+                <td>
+                    <div class="radio">
+                        <input type="radio" name="speechTherapy" id="speechTherapyNo" value="no">
+                        <label for="speechTherapyNo">
+                            No 
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <input type="radio" name="speechTherapy" id="speechTherapyYes" value="yes">
+                        <label for="speechTherapyYes">
+                            Yes
+                        </label>
+                      Explain: 
+                        <input type="text" id="speechTherapyExplain"></input>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    What year did your restriction with speech begin?
+                </td>
+                <td>
+                    <select name="speechBegin">
+                    '; 
+                    
+                        for ($i = date('Y') ; $i > 1950; $i--) {
+                            echo "<option>$i</option>";
+                        };
+                    
+                    echo '</select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Was your speech restriction resolved? If yes please indicate what year.
+                </td>
+                <td>
+                    <select name="speechResolve">
+                    '; 
+                    
+                        for ($i = date('Y') ; $i > 1950; $i--) {
+                            echo "<option>$i</option>";
+                        };
+                    
+                    echo '</select>
+                </td>
+            </tr>
         </table>
     </div>
     ';
