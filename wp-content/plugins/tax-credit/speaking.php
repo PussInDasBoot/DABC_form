8a0dc52c0818395f96129351536c757926900428
PussInDasBoot <mroberts@adacado.com>
2018-06-20 11:53:47 +0200

    Updated speaking

--- wp-content/plugins/tax-credit/speaking.php
+++ wp-content/plugins/tax-credit/speaking.php
@@ -40,194 +40,202 @@ function speaking_registration_function() {
                     </div>
                 </td>
             </tr>
-            <tr id="speechSevereRestrFreq" hidden>
-                <td>
-                    How often does this happen?
-                </td>
-                <td>
-                    <div class="radio">
-                        <input type="radio" name="speechRestrFreq" id="speechRestrFreqRarely" value="rarely">
-                        <label for="speechRestrFreqRarely">
-                            Rarely
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechRestrFreq" id="speechRestrFreqSome" value="some">
-                        <label for="speechRestrFreqSome">
-                            Some of the time 
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechRestrFreq" id="speechRestrFreqVery" value="very">
-                        <label for="speechRestrFreqVery">
-                            At least 90% of the time
-                        </label>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    When you speak with your doctor, your doctor must ask you to repeat words and sentences. How long does it take to communicate your needs?
-                </td>
-                <td>
-                    <div class="radio">
-                        <input type="radio" name="speechLonger" id="speechLongerThree" value="three">
-                        <label for="speechLongerThree">
-                            At least three times as long, compared to an average person my age without my restrictions.
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechLonger" id="speechLongerTwo" value="two">
-                        <label for="speechLongerTwo">
-                            At least two times as long, compared to an average person my age without my restrictions.
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechLonger" id="speechLongerLittle" value="little">
-                        <label for="speechLongerLittle">
-                            A little bit longer, compared to an average person my age without my restrictions.
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechLonger" id="speechLongerSame" value="same">
-                        <label for="speechLongerSame">
-                            The same amount of time, compared to an average person my age without my restrictions.
-                        </label>
-                    </div>
-                <td>
-            </tr>
-            <tr>
-                <td>
-                    What happens when you speak?
-                </td>
-                <td>
-                    <div>
-                        <input type="checkbox" name="speechDescribe" id="speechDescribeVol" value="vol">
-                        <label for="speechDescribeVol">
-                            Low-volume
-                        </label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechDescribe" id="speechDescribeSlur" value="slur">
-                        <label for="speechDescribeSlur">
-                            Slurring
-                        </label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechDescribe" id="speechDescribeStut" value="stut">
-                        <label for="speechDescribeStut">
-                            Stuttering
-                        </label>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Do you use any appropriate therapy, medication or devices to assist you?
-                </td>
-                <td>
-                    <div class="radio">
-                        <input type="radio" name="speechTherapyYesNo" id="speechTherapyNo" value="no">
-                        <label for="speechTherapyNo">
-                            No 
-                        </label>    
-                    </div>
-                    <div class="radio">
-                        <input type="radio" name="speechTherapyYesNo" id="speechTherapyYes" value="yes">
-                        <label for="speechTherapyYes">
-                            Yes 
-                        </label>    
-                    </div>
-                    <div id="speechTherapyAssist" hidden>
+            <tbody id="speechUnable" hidden>
+                <tr>
+                    <td>
+                        How often does this happen?
+                    </td>
+                    <td>
+                        <div class="radio">
+                            <input type="radio" name="speechRestrFreq" id="speechRestrFreqRarely" value="rarely">
+                            <label for="speechRestrFreqRarely">
+                                Rarely
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechRestrFreq" id="speechRestrFreqSome" value="some">
+                            <label for="speechRestrFreqSome">
+                                Some of the time 
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechRestrFreq" id="speechRestrFreqVery" value="very">
+                            <label for="speechRestrFreqVery">
+                                At least 90% of the time
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        When you speak with your doctor, your doctor must ask you to repeat words and sentences. How long does it take to communicate your needs?
+                    </td>
+                    <td>
+                        <div class="radio">
+                            <input type="radio" name="speechLonger" id="speechLongerThree" value="three">
+                            <label for="speechLongerThree">
+                                At least three times as long, compared to an average person my age without my restrictions.
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechLonger" id="speechLongerTwo" value="two">
+                            <label for="speechLongerTwo">
+                                At least two times as long, compared to an average person my age without my restrictions.
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechLonger" id="speechLongerLittle" value="little">
+                            <label for="speechLongerLittle">
+                                A little bit longer, compared to an average person my age without my restrictions.
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechLonger" id="speechLongerSame" value="same">
+                            <label for="speechLongerSame">
+                                The same amount of time, compared to an average person my age without my restrictions.
+                            </label>
+                        </div>
+                    <td>
+                </tr>
+                <tr>
+                    <td>
+                        What happens when you speak?
+                    </td>
+                    <td>
                         <div>
-                            <input type="checkbox" name="speechTherapyAssist" id="speechTherapyDevices" value="devices">
-                            <label for="speechTherapyDevices">
-                                I use devices
+                            <input type="checkbox" name="speechDescribe" id="speechDescribeVol" value="vol">
+                            <label for="speechDescribeVol">
+                                Low-volume
+                            </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="speechDescribe" id="speechDescribeSlur" value="slur">
+                            <label for="speechDescribeSlur">
+                                Slurring
+                            </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="speechDescribe" id="speechDescribeStut" value="stut">
+                            <label for="speechDescribeStut">
+                                Stuttering
+                            </label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="speechDescribe" id="speechDescribeEnun" value="enun">
+                            <label for="speechDescribeEnun">
+                                Poor enunciation
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Do you use any appropriate therapy, medication or devices to assist you?
+                    </td>
+                    <td>
+                        <div class="radio">
+                            <input type="radio" name="speechTherapyYesNo" id="speechTherapyNo" value="no">
+                            <label for="speechTherapyNo">
+                                No 
+                            </label>    
+                        </div>
+                        <div class="radio">
+                            <input type="radio" name="speechTherapyYesNo" id="speechTherapyYes" value="yes">
+                            <label for="speechTherapyYes">
+                                Yes 
+                            </label>    
+                        </div>
+                        <div id="speechTherapyAssist" hidden>
+                            <div>
+                                <input type="checkbox" name="speechTherapyAssist" id="speechTherapyDevices" value="devices">
+                                <label for="speechTherapyDevices">
+                                    I use devices
+                                </label>    
+                            </div>
+                            <div>
+                                <input type="checkbox" name="speechTherapyAssist" id="speechTherapyTherapy" value="therapy">
+                                <label for="speechTherapyTherapy">
+                                    I receive therapy (physio, etc)
+                                </label>    
+                            </div>
+                            <div>
+                                <input type="checkbox" name="speechTherapyAssist" id="speechTherapyMedication" value="medication">
+                                <label for="speechTherapyMedication">
+                                    I take medication
+                                </label>    
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr id="speechDevices" hidden>
+                    <td>
+                        Please indicate the devices you use.
+                    </td>
+                    <td>
+                        <div>
+                            <input type="checkbox" name="speechDevices" id="speechDevicesAdaptive" value="adaptive">
+                            <label for="speechDevicesAdaptive">
+                                Adaptive equipment 
                             </label>    
                         </div>
                         <div>
-                            <input type="checkbox" name="speechTherapyAssist" id="speechTherapyTherapy" value="therapy">
-                            <label for="speechTherapyTherapy">
-                                I receive therapy (physio, etc)
+                            <input type="checkbox" name="speechDevices" id="speechDevicesOther" value="other">
+                            <label for="speechDevicesOther">
+                                Other:
+                            </label>
+                            <input type="text" id="speechDevicesOtherInput"></input>     
+                        </div>
+                    </td>
+                </tr>
+                <tr id="speechTherapy" hidden>
+                    <td>
+                        Please indicate the therapy you receive.
+                    </td>
+                    <td>
+                        <div>
+                            <input type="checkbox" name="speechTherapy" id="speechTherapyPhysio" value="physio">
+                            <label for="speechTherapyPhysio">
+                                Physiotherapy
                             </label>    
                         </div>
                         <div>
-                            <input type="checkbox" name="speechTherapyAssist" id="speechTherapyMedication" value="medication">
-                            <label for="speechTherapyMedication">
-                                I take medication
+                            <input type="checkbox" name="speechTherapy" id="speechTherapyChiro" value="chiro">
+                            <label for="speechTherapyChiro">
+                                Chiropractic
                             </label>    
                         </div>
-                    </div>
-                </td>
-            </tr>
-            <tr id="speechDevices" hidden>
-                <td>
-                    Please indicate the devices you use.
-                </td>
-                <td>
-                    <div>
-                        <input type="checkbox" name="speechDevices" id="speechDevicesAdaptive" value="adaptive">
-                        <label for="speechDevicesAdaptive">
-                            Adaptive equipment 
-                        </label>    
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechDevices" id="speechDevicesOther" value="other">
-                        <label for="speechDevicesOther">
-                            Other:
-                        </label>
-                        <input type="text" id="speechDevicesOtherInput"></input>     
-                    </div>
-                </td>
-            </tr>
-            <tr id="speechTherapy" hidden>
-                <td>
-                    Please indicate the therapy you receive.
-                </td>
-                <td>
-                    <div>
-                        <input type="checkbox" name="speechTherapy" id="speechTherapyPhysio" value="physio">
-                        <label for="speechTherapyPhysio">
-                            Physiotherapy
-                        </label>    
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechTherapy" id="speechTherapyChiro" value="chiro">
-                        <label for="speechTherapyChiro">
-                            Chiropractic
-                        </label>    
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechTherapy" id="speechTherapyMassage" value="massage">
-                        <label for="speechTherapyMassage">
-                            Massage
-                        </label>    
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechTherapy" id="speechTherapyOcc" value="occ">
-                        <label for="speechTherapyOcc">
-                            Occupational Therapy
-                        </label>    
-                    </div>
-                    <div>
-                        <input type="checkbox" name="speechTherapy" id="speechTherapyOther" value="other">
-                        <label for="speechTherapyOther">
-                            Other:
-                        </label>
-                        <input type="text" id="speechTherapyOtherInput"></input>     
-                    </div>
-                </td>
-            </tr>
-            <tr id="speechMedication" hidden>
-                <td>
-                    Please indicate what medication you take to address your restriction.
-                </td>
-                <td>
-                    <div id="speechMedication">
-                        <input type="text" id="speechMedicationInput"></input>  
-                    </div>
-                </td>
-            </tr>
+                        <div>
+                            <input type="checkbox" name="speechTherapy" id="speechTherapyMassage" value="massage">
+                            <label for="speechTherapyMassage">
+                                Massage
+                            </label>    
+                        </div>
+                        <div>
+                            <input type="checkbox" name="speechTherapy" id="speechTherapyOcc" value="occ">
+                            <label for="speechTherapyOcc">
+                                Occupational Therapy
+                            </label>    
+                        </div>
+                        <div>
+                            <input type="checkbox" name="speechTherapy" id="speechTherapyOther" value="other">
+                            <label for="speechTherapyOther">
+                                Other:
+                            </label>
+                            <input type="text" id="speechTherapyOtherInput"></input>     
+                        </div>
+                    </td>
+                </tr>
+                <tr id="speechMedication" hidden>
+                    <td>
+                        Please indicate what medication you take to address your restriction.
+                    </td>
+                    <td>
+                        <div id="speechMedication">
+                            <input type="text" id="speechMedicationInput"></input>  
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
             <tr>
                 <td>
                     What year did your restriction begin?
